782da66db4f0de766bc0b1906d85136d52d0834d
brunogoliveiira-rgb <brunogoliveiira@gmail.com>
2026-05-19 08:18:09 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'art-conf-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
